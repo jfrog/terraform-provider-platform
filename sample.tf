452115ba0e2d73e7193b8e4ecbe5b4af1fc195b5
Alex Hung <alexh@jfrog.com>
2023-12-15 09:39:53 -0800

    Use heredoc for source_code in sample.tf and doc example
    Add Artifactory provider to sample.tf

--- sample.tf
+++ sample.tf
@@ -2,14 +2,19 @@ terraform {
   required_providers {
     platform = {
       source  = "registry.terraform.io/jfrog/platform"
-      version = "1.0.0"
+      version = "1.0.3"
+    }
+
+    artifactory = {
+      source  = "registry.terraform.io/jfrog/artifactory"
+      version = "10.0.0"
     }
   }
 }
 
 variable "jfrog_url" {
   type = string
-  default = "http://localhost:8081"
+  default = "https://partnership.jfrog.io"
 }
 
 provider "platform" {
@@ -17,16 +22,29 @@ provider "platform" {
   // supply JFROG_ACCESS_TOKEN as env var
 }
 
+resource "artifactory_local_generic_repository" "my-generic-local" {
+  key = "my-generic-local"
+}
+
 resource "platform_workers_service" "my-workers-service" {
   key         = "my-workers-service"
   enabled     = true
   description = "My workers service"
-  source_code = "export default async (context: PlatformContext, data: BeforeDownloadRequest): Promise<BeforeDownloadResponse> => { console.log(await context.clients.platformHttp.get('/artifactory/api/system/ping')); console.log(await axios.get('https://my.external.resource')); return { status: 'DOWNLOAD_PROCEED', message: 'proceed', } }"
+  source_code = <<EOT
+export default async (context: PlatformContext, data: BeforeDownloadRequest): Promise<BeforeDownloadResponse> => {
+  console.log(await context.clients.platformHttp.get('/artifactory/api/system/ping'));
+  console.log(await axios.get('https://my.external.resource'));
+  return {
+    status: 'DOWNLOAD_PROCEED',
+    message: 'proceed',
+  }
+}
+EOT
   action      = "BEFORE_DOWNLOAD"
 
   filter_criteria = {
     artifact_filter_criteria = {
-      repo_keys = ["my-generic-local"]
+      repo_keys = [artifactory_local_generic_repository.my-generic-local.key]
     }
   }
 
