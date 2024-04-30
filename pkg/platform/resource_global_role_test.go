4f2f084b1a4102637bd992dcd7d8f674bd0783fe
Alex Hung <alexh@jfrog.com>
2024-04-30 16:14:12 -0700

    Add name change tests

--- pkg/platform/resource_global_role_test.go
+++ pkg/platform/resource_global_role_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/plancheck"
 	"github.com/jfrog/terraform-provider-shared/testutil"
 )
 
@@ -81,6 +82,65 @@ func TestAccGlobalRole_full(t *testing.T) {
 				ImportStateId:                        roleName,
 				ImportStateVerify:                    true,
 				ImportStateVerifyIdentifierAttribute: "name",
+			},
+		},
+	})
+}
+
+func TestAccGlobalRole_name_change(t *testing.T) {
+	_, fqrn, roleName := testutil.MkNames("test-global-role", "platform_global_role")
+
+	temp := `
+	resource "platform_global_role" "{{ .name }}" {
+		name         = "{{ .name }}"
+		description  = "Test description"
+		type         = "{{ .type }}"
+		environments = ["{{ .environment }}"]
+		actions      = ["{{ .action }}"]
+	}`
+
+	testData := map[string]string{
+		"name":        roleName,
+		"type":        "CUSTOM_GLOBAL",
+		"environment": "DEV",
+		"action":      "READ_REPOSITORY",
+	}
+
+	config := testutil.ExecuteTemplate(roleName, temp, testData)
+
+	nameChangeTemp := `
+	resource "platform_global_role" "{{ .name }}" {
+		name         = "foobar"
+		description  = "Test description"
+		type         = "{{ .type }}"
+		environments = ["{{ .environment }}"]
+		actions      = ["{{ .action }}"]
+	}`
+
+	updatedConfig := testutil.ExecuteTemplate(roleName, nameChangeTemp, testData)
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:                 func() { testAccPreCheck(t) },
+		ProtoV6ProviderFactories: testAccProviders(),
+		Steps: []resource.TestStep{
+			{
+				Config: config,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr(fqrn, "name", testData["name"]),
+					resource.TestCheckResourceAttr(fqrn, "type", testData["type"]),
+					resource.TestCheckResourceAttr(fqrn, "environments.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "environments.0", "DEV"),
+					resource.TestCheckResourceAttr(fqrn, "actions.#", "1"),
+					resource.TestCheckResourceAttr(fqrn, "actions.0", "READ_REPOSITORY"),
+				),
+			},
+			{
+				Config: updatedConfig,
+				ConfigPlanChecks: resource.ConfigPlanChecks{
+					PreApply: []plancheck.PlanCheck{
+						plancheck.ExpectResourceAction(fqrn, plancheck.ResourceActionDestroyBeforeCreate),
+					},
+				},
 			},
 		},
 	})
