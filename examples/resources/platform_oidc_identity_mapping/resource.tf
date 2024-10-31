08b8839b7563266d37a6f3bc3b9741bb39fdfc30
Alex Hung <alexh@jfrog.com>
2024-10-31 13:12:04 -0700

    Add 'username_pattern' and 'groups_pattern' attributes
    Add validation to 'username' attribute to ensure 'scope' attribute is also 
    
    Don't set 'scope' attribute unless it's not empty string
    
    Update documentation to include the same

--- examples/resources/platform_oidc_identity_mapping/resource.tf
+++ examples/resources/platform_oidc_identity_mapping/resource.tf
@@ -34,3 +34,59 @@ resource "platform_oidc_identity_mapping" "my-github-oidc-group-identity-mapping
     expires_in = 7200
   }
 }
+
+resource "platform_oidc_identity_mapping" "my-github-oidc-project-roles-identity-mapping" {
+  name          = "my-github-oidc-project-role-identity-mapping"
+  description   = "My GitHub OIDC Project role identity mapping"
+  provider_name = "my-github-oidc-configuration"
+  priority      = 1
+
+  claims_json = jsonencode({
+    "sub" = "repo:humpty/access-oidc-poc:ref:refs/heads/main",
+    "workflow_ref" = "humpty/access-oidc-poc/.github/workflows/job.yaml@refs/heads/main"
+  })
+
+  token_spec = {
+    scope      = "applied-permissions/roles:my-project:\"Project Admin\",\"Developer\""
+    audience   = "jfrt@* jfac@* jfmc@* jfmd@* jfevt@* jfxfer@* jflnk@* jfint@* jfwks@*"
+    expires_in = 7200
+  }
+
+  project_key = "my-project"
+}
+
+resource "platform_oidc_identity_mapping" "my-github-oidc-username-pattern-identity-mapping" {
+  name          = "my-github-oidc-username-pattern-identity-mapping"
+  description   = "My GitHub OIDC username pattern identity mapping"
+  provider_name = "my-github-oidc-configuration"
+  priority      = 1
+
+  claims_json = jsonencode({
+    "sub" = "repo:humpty/access-oidc-poc:ref:refs/heads/main",
+    "workflow_ref" = "humpty/access-oidc-poc/.github/workflows/job.yaml@refs/heads/main"
+  })
+
+  token_spec = {
+    username_pattern = "{{user}}"
+    audience         = "*@*"
+    expires_in       = 7200
+  }
+}
+
+resource "platform_oidc_identity_mapping" "my-github-oidc-groups-pattern-identity-mapping" {
+  name          = "my-github-oidc-groups-pattern-identity-mapping"
+  description   = "My GitHub OIDC groups pattern identity mapping"
+  provider_name = "my-github-oidc-configuration"
+  priority      = 1
+
+  claims_json = jsonencode({
+    "sub" = "repo:humpty/access-oidc-poc:ref:refs/heads/main",
+    "workflow_ref" = "humpty/access-oidc-poc/.github/workflows/job.yaml@refs/heads/main"
+  })
+
+  token_spec = {
+    groups_pattern = "{{group}}"
+    audience       = "*@*"
+    expires_in     = 7200
+  }
+}
